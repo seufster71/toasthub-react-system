c7d696492ccca26ac1cb5c1de00c03ded7c5dc18
Edward Seufert <seufster@gmail.com>
2022-01-31 21:33:50 -0500

    Convert to hooks

--- system-container.js
+++ system-container.js
@@ -2,11 +2,9 @@
 * Author Edward Seufert
 */
 'use strict';
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import { Switch, Route, withRouter, Redirect} from "react-router";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import * as systemActions from './system-actions';
 import LoginContainer from '../core/usermgnt/login-container';
 import StatusView from '../coreView/status/status-view';
@@ -20,44 +18,46 @@ import ApplicationContainer from './application/application-container';
 import fuLogger from '../core/common/fu-logger';
 import {PrivateRoute} from '../core/common/utils';
 
-class SystemContainer extends Component {
-  constructor(props) {
-		super(props);
-    this.changeTab = this.changeTab.bind(this);
-	}
+function SystemContainer() {
+	const member = useSelector((state) => state.member);
+	const session = useSelector((state) => state.session);
+	const appMenus = useSelector((state) => state.appMenus);
+	const appPrefs = useSelector((state) => state.appPrefs);
+	const dispatch = useDispatch();
+	const location = useLocation();
+	const navigate = useNavigate();
+  	
+	useEffect(() => {
+    	dispatch(systemActions.initSystem());
+  	}, []);
 
-  componentDidMount() {
-    this.props.actions.initSystem();
-  }
+	const changeTab = (code,index) => {
+      navigate(index);
+  	}
 
-  changeTab(code,index) {
-      this.props.history.replace(index);
-  }
-
-  render() {
-    fuLogger.log({level:'TRACE',loc:'SystemContainer::render',msg:"path "+ this.props.history.location.pathname});
+    fuLogger.log({level:'TRACE',loc:'SystemContainer::render',msg:"path "+ location.pathname});
 
     let myMenus = [];
-    if (this.props.appMenus != null && this.props.appMenus[this.props.appPrefs.systemMenu] != null) {
-      myMenus = this.props.appMenus[this.props.appPrefs.systemMenu];
+    if (appMenus != null && appMenus[appPrefs.systemMenu] != null) {
+      myMenus = appMenus[appPrefs.systemMenu];
     }
     let myPermissions = {};
-    if (this.props.member != null && this.props.member.user != null && this.props.member.user.permissions != null) {
-      myPermissions = this.props.member.user.permissions;
+    if (member != null && member.user != null && member.user.permissions != null) {
+      myPermissions = member.user.permissions;
     }
     if (myMenus.length > 0) {
       return (
         <SystemView>
-          <NavigationView appPrefs={this.props.appPrefs} permissions={myPermissions}
-          menus={myMenus} changeTab={this.changeTab} activeTab={this.props.history.location.pathname}/>
+          <NavigationView appPrefs={appPrefs} permissions={myPermissions}
+          menus={myMenus} changeTab={changeTab} activeTab={location.pathname}/>
           <StatusView/>
-          <Switch>
+          <Routes>
             <Route exact path="/" component={DashboardContainer} />
             <Route exact path="/system" component={DashboardContainer} />
             <PrivateRoute path="/system-clientdomain" component={ClientDomainContainer} permissions={myPermissions} code="SCD" pathto="/access-denied"/>
             <PrivateRoute path="/system-services" component={ServicesContainer} permissions={myPermissions} code="SSC" pathto="/access-denied"/>
             <PrivateRoute path="/system-application" component={ApplicationContainer} permissions={myPermissions} code="SA" pathto="/access-denied"/>
-          </Switch>
+          </Routes>
         </SystemView>
       );
     } else {
@@ -66,25 +66,7 @@ class SystemContainer extends Component {
         </SystemView>
       );
     }
-  }
 }
 
-SystemContainer.propTypes = {
-	appPrefs: PropTypes.object.isRequired,
-	appMenus: PropTypes.object,
-	lang: PropTypes.string,
-  session: PropTypes.object,
-  member: PropTypes.object,
-	actions: PropTypes.object,
-  history: PropTypes.object
-};
 
-function mapStateToProps(state, ownProps) {
-  return {appPrefs:state.appPrefs, appMenus:state.appMenus, lang:state.lang, session:state.session, member:state.member};
-}
-
-function mapDispatchToProps(dispatch) {
-  return { actions:bindActionCreators(systemActions,dispatch) };
-}
-
-export default withRouter(connect(mapStateToProps,mapDispatchToProps)(SystemContainer));
+export default SystemContainer;
